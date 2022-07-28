e2af391a399d4979e3232f8beba5a45ae7712ff3
Silver Rooney <silver.rooney.dev@gmail.com>
2022-07-28 11:49:19 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Silver on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
